f73d60a3f4487461f46acb333f489df5cc0f968f
Henryysz <107366022+Henryysz@users.noreply.github.com>
2022-10-01 18:27:00 -0300

    ativiaddes dia 01/10

--- atividade1.py
+++ atividade1.py
@@ -1,25 +1,62 @@
-class Usuario:
-    def __init__(self, login, senha):
-        self.__login = login
-        self.__senha = senha
+import mysql.connector
+import os 
+from dotenv import load_dotenv
 
-        #getter
-        @property
-        def login(self):
-            return self.__login
+load_dotenv('./.env')
 
-        #setter
-        @login.setter
-        def login(self, novo_login):
-            self.__login = novo_login
+try:
+    conn = mysql.connector.connect(
+        host=os.getenv('HOST'),
+        port=os.getenv('PORT'),
+        user=os.getenv('USER'),
+        passwd=os.getenv('PASSWD'),
+        db=os.getenv('DB')
+    )
+except Exception as err:
+    raise err('Deu ruim')
+else:
+    #print(conn.is_connected())
+    cur = conn.cursor()
 
-        @property
-        def senha(self):
-            return self.__senha
+query = 'SELECT LOGIN FROM USERS'
+cur.execute(query)
+users = [ user[0] for user in cur.fetchall()]
+print(users)
 
-        @senha.setter
-        def senha(self, nova_senha):
-            if len(nova_senha) < 4:
-                print('Senha inválida. Tamanho mínimo de 4')
-            self.__senha = nova_senha
-            return 'Senha alterada com sucesso'
+print('Qual registro deseja atualizar?')
+for pos, login in enumerate(users, start=1):
+    print(pos, login, sep= ' : ')
+opcao = int(input('Opção: '))
+
+query = f'DELETE FROM USERS WHERE LOGIN="{users[opcao -1]}"'
+cur.execute(query)
+conn.commit()
+
+# nome = input('Novo nome: ').title()
+# query = f'UPDATE USERS SET FULL_NAME="{nome}" WHERE login="{users[opcao - 1]}"'
+# cur.execute(query)
+# conn.commit()
+
+# login = input('Login: ').lower()
+
+# while login in users:
+#     print(f'Login {login} já existe')
+#     login = input('Login: ').lower()
+    
+# if login not in users:
+#     nome = input('Nome: ').title()
+#     query = f'INSERT INTO USERS VALUES ("{login}", "{nome}")'
+#     cur.execute(query)
+#     conn.commit()
+
+
+
+# query = 'SELECT * FROM USERS'
+# cur.execute(query)
+# retorno = cur.fetchall()
+
+# for login, nome in retorno:
+#     print(login, nome, sep= ' ----- ')
+
+
+
